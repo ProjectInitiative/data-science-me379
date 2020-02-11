4b27182351337ad9ab6bf4f8a9c5fbfe5d28aa89
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2020-02-11 11:55:29 -0600

    Lecture 2/11/2020
    - Learned about feature engineering
    - How to figure out to deal with nan types

--- functions.py
+++ functions.py
@@ -24,3 +24,19 @@ def currency(num):
     import locale
     _ = locale.setlocale(locale.LC_ALL, '')
     return str(locale.currency(num, grouping=True))
+
+def build_score_randomforest(x_train, y_train, x_test, y_test):
+    from sklearn.ensemble import RandomForestRegressor
+    from sklearn.metrics import mean_absolute_error
+    # Define
+    model = RandomForestRegressor(random_state=1)
+
+    # Fit
+    _ = model.fit(x_train, y_train)
+
+    # Predict
+    y_test_pred = model.predict(x_test)
+
+    # Evaluate
+    mae = mean_absolute_error(y_test, y_test_pred)
+    return mae
